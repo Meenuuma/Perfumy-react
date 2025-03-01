03b2cc7fb7e65dd72b3c9e9224cabc17280e31f6
meenuuma <meenakshiuma1989@gmail.com>
2025-03-01 07:35:39 -0600

    update project

---
+++ src/components/Product.js
@@ -0,0 +1,27 @@
+//product component
+function Product() {
+  return (
+    <div class="products">
+
+      <div class="box">
+
+        <img src="https://plumgoodness.com/cdn/shop/files/MKD_01.jpg?v=1728452056&width=460" alt="perfume"></img>
+        <p>Fogg Wood Extreme, Eau De Parfum, Long-lasting Perfume, 100ml</p>
+      </div>
+
+
+      <div class="box">
+        <img src="https://marketplace.canva.com/EAGSec-fmik/2/0/1600w/canva-pink-elegant-special-sale-perfume-instagram-post-Uvwn1uyqzaM.jpg" alt="perfume"></img>
+        <p>Fogg Wood Extreme, Eau De Parfum, Long-lasting Perfume, 100ml</p>
+      </div>
+      <div class="box">
+        <img src="https://cdn.shopify.com/s/files/1/1445/2216/files/SaltAir_Collection_775x.progressive.jpg" alt="perfume"></img>
+        <p>Designer Club-Be Spoke by Perfume Lounge Men's Perfume | Premium Long Lasting Perfume</p>
+      </div>
+
+
+
+    </div>
+  )
+}
+export default Product
